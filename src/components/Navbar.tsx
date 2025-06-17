b4368243ccfc99225eab66cc2f6d429259c31f45
4lex42 <a.laroy@students.ephec.be>
2025-06-17 17:34:56 +0200

    fix Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -47,7 +47,7 @@ export default function Navbar() {
               </Link>
             </li> */}
             <li>
-              <Link href="/Dashboard" className="text-gray-700 hover:text-blue-600">
+              <Link href="/dashboard" className="text-gray-700 hover:text-blue-600">
                 Dashboard
               </Link>
             </li>
